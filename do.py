cfff3141f4283b997f97e6013640945c088cc82e
201721123007    李琴 <37699592+Liqin1999@users.noreply.github.com>
2020-05-27 22:35:47 +0800

    Delete do.py

--- do.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("python ./bleu.py")
-os.system("python ./couplet.py")
-os.system("python ./iwslt.py")
-os.system("python ./model.py")
-os.system("python ./nmt.py")
-os.system("python ./reader.py")
-os.system("python ./seq2seq.py")
-os.system("python ./server.py")
